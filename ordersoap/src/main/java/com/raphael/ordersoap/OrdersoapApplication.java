4a5185b1cb63dbbf29702b3dc6cd0cf48bfb1ec4
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2026-03-26 11:23:36 -0300

    Commit inicial do projeto

---
+++ ordersoap/src/main/java/com/raphael/ordersoap/OrdersoapApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.ordersoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersoapApplication.class, args);
+	}
+
+}
---
+++ ordersoap/src/test/java/com/raphael/ordersoap/OrdersoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.ordersoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
